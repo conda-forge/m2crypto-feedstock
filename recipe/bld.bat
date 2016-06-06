01e872d915be763f7f23e9a4e57e9b752cab0a2f
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-06-05 22:20:59 -0700

    Use M2CryptoWin64 for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 # Use PREFIX to use conda openssl
-"%PYTHON%" setup.py build_ext --openssl %LIBRARY_PREFIX%
+"%PYTHON%" setup.py build build_ext --openssl %LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
