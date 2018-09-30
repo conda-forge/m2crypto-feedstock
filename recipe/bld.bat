4458d7bdbae5fe2d11757b27a53a8efacdb919d5
Duncan Macleod <duncanmmacleod@gmail.com>
2018-09-30 17:23:07 +0100

    Update to m2crypto 0.30.1 (#21)
    * recipe: attempt at build for 0.30.1
    
    I have no idea what I'm doing
    
    * MNT: Re-rendered with conda-smithy 3.1.12 and pinning 2018.09.20
    
    * bld.bat: fixed openssl prefix path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py build build_ext --openssl="%LIBRARY_PREFIX%"
+if errorlevel 1 exit 1
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
