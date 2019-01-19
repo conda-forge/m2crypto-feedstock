3a3200f0e42f2f98106da58dfbc610d941bd01ca
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-01-19 09:50:09 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# the setup.py script is wrongly using cpp
+ln -s "${CXX}" "${$BUILD_PREFIX}/bin"
+
 # Use PREFIX to use conda openssl
 $PYTHON setup.py build build_ext --openssl $PREFIX
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
