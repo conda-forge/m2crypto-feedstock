1dfa7243b1d9dbc07e31f98cd87fbc491509ab57
Duncan Macleod <duncan.macleod@ligo.org>
2021-01-27 16:45:44 +0000

    use patch to configure openssl
    rather than build scripts, this seems cleaner to me

--- recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-"%PYTHON%" setup.py build build_ext --openssl="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Use PREFIX to use conda openssl
-$PYTHON setup.py build build_ext --openssl $PREFIX
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
